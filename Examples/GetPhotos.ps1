caa8aebbfdc4a8d6a6cde0d79192e15304167186
vmouse <vlad.baginsky@gmail.com>
2014-08-04 10:58:42 +0400

    economic Get call

--- Examples/GetPhotos.ps1
+++ Examples/GetPhotos.ps1
@@ -6,7 +6,7 @@
 $srv = New-WebServiceProxy -uri "http://support/_srv/wsEmployee.asmx?wsdl" -UseDefaultCredential
 
 $srv.ListNames(0,0) | foreach {
-	$emp = $srv.Get($_.emp_id)
+	$emp = $srv.Get($_.emp_id, $true)
 	if ($emp.emp_photo.Count -gt 0) {
 		[IO.File]::WriteAllBytes(("c:\temp\Photos\{0}.jpg" -f ($emp.SelectName -replace "\s+","_")), $emp.emp_photo)
 	}
